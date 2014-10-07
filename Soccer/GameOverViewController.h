dd59903277add389a5ccee69d708b2f143959b76
TonyHongnanYang <hongnanyang2013@gmail.com>
2014-10-07 16:19:27 +1100

    add game overviewcotroller

---
+++ Soccer/GameOverViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameOverViewController.h
+//  Soccer
+//
+//  Created by Hongnan Yang on 2014-10-7.
+//  Copyright (c) 2014年 hao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameOverViewController : UIViewController
+
+@end
---
+++ Soccer/GameOverViewController.m
@@ -0,0 +1,37 @@
+//
+//  GameOverViewController.m
+//  Soccer
+//
+//  Created by Hongnan Yang on 2014-10-7.
+//  Copyright (c) 2014年 hao. All rights reserved.
+//
+
+#import "GameOverViewController.h"
+
+@interface GameOverViewController ()
+
+@end
+
+@implementation GameOverViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

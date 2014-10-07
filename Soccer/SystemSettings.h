f7af993cd14a7f2cc1d9c330f827044248b9ff89
TonyHongnanYang <hongnanyang2013@gmail.com>
2014-10-07 18:07:06 +1100

    develop database accessor on client side

---
+++ Soccer/SystemSettings.h
@@ -0,0 +1,14 @@
+
+//
+//  SystemSettings.h
+//  Soccer
+//
+//  Created by Hongnan Yang on 2014-10-7.
+//  Copyright (c) 2014年 hao. All rights reserved.
+//
+
+#ifndef Soccer_SystemSettings_h
+#define Soccer_SystemSettings_h
+#define URLADDRESS  @ "http://localhost/SoccerGame/DatabaseAccess.php"
+
+#endif

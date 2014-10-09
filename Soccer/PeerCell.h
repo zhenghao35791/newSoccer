f3cc57d0ef390ebfd8cb2246aab943d73d086b3c
josh zhu <joshuazhu1989@gmail.com>
2014-10-09 14:10:15 +1100

    add peer

---
+++ Soccer/PeerCell.h
@@ -0,0 +1,13 @@
+//
+//  PeerCell.h
+//  Snap
+//
+//  Created by Finguitar on 27/09/2014.
+//  Copyright (c) 2014 Hollance. All rights reserved.
+// sasdasd
+
+#import <UIKit/UIKit.h>
+
+@interface PeerCell : UITableViewCell
+
+@end
